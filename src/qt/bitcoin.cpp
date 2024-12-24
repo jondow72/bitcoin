e8752accb4017166bcb7269e6d8950530f7cd924
jondow72 <77569928+jondow72@users.noreply.github.com>
2024-12-24 10:39:16 +0100

    Update bitcoin.cpp

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -122,7 +122,7 @@ int main(int argc, char *argv[])
     QTextCodec::setCodecForCStrings(QTextCodec::codecForTr());
 #endif
 
-    Q_INIT_RESOURCE(magi);
+    Q_INIT_RESOURCE(bitcoin);
     
     QApplication app(argc, argv);
 #if QT_VERSION > 0x050100
